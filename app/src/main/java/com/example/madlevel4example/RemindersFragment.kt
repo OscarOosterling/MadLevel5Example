abd23d279ecf175045b5e4846a71b61395c65184
oscar <oscar.oosterling@hva.nl>
2020-10-02 17:54:50 +0200

    completed step 2

--- app/src/main/java/com/example/madlevel4example/RemindersFragment.kt
+++ app/src/main/java/com/example/madlevel4example/RemindersFragment.kt
@@ -20,6 +20,8 @@ import kotlinx.android.synthetic.main.fragment_reminders.*
  */
 class RemindersFragment : Fragment() {
 
+    private lateinit var reminderRepository: ReminderRepository
+
     private val reminders= arrayListOf<Reminder>()
     private val reminderAdapter = ReminderAdapter(reminders)
     override fun onCreateView(
@@ -34,9 +36,17 @@ class RemindersFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         initViews()
         observeAddReminderResult()
+        reminderRepository = ReminderRepository(requireContext())
+        getRemindersFromDatabase()
 
     }
 
+    private fun getRemindersFromDatabase(){
+        val reminders = reminderRepository.getAllReminders()
+        this@RemindersFragment.reminders.clear()
+        this@RemindersFragment.reminders.addAll(reminders)
+        reminderAdapter.notifyDataSetChanged()
+    }
     private fun initViews() {
         rvReminders.layoutManager = LinearLayoutManager(context,RecyclerView.VERTICAL,false)
         rvReminders.adapter = reminderAdapter
@@ -48,8 +58,8 @@ class RemindersFragment : Fragment() {
             bundle.getString(BUNDLE_REMINDER_KEY)?.let {
                 val reminder = Reminder(it)
 
-                reminders.add(reminder)
-                reminderAdapter.notifyDataSetChanged()
+                reminderRepository.insertReminder(reminder)
+                getRemindersFromDatabase()
             } ?: Log.e("ReminderFragment", "Request triggered, but empty reminder text!")
 
         }
@@ -72,8 +82,11 @@ class RemindersFragment : Fragment() {
             // Callback triggered when a user swiped an item.
             override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
                 val position = viewHolder.adapterPosition
-                reminders.removeAt(position)
-                reminderAdapter.notifyDataSetChanged()
+                
+                val reminderToDelete=reminders[position]
+                reminderRepository.deleteReminder(reminderToDelete)
+                getRemindersFromDatabase()
+
             }
         }
         return ItemTouchHelper(callback)
